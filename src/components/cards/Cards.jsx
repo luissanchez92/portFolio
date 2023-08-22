32c9bd2bf4c211dc5d65de7b1030aaa46b8ab695
luissanchez92 <luisjosanchez69@gmail.com>
2023-08-21 22:50:49 -0300

    add

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,5 +1,7 @@
 import imageShopConnect from '../../assets/shopConnect.png'
 import cotizador from '../../assets/cotizador.png'
+import videoGames from '../../assets/videoGames.png'
+import porfolio from '../../assets/porfolio.png'
 import Card from '../card/Card'
 import style from './Cards.module.css'
 
@@ -19,6 +21,22 @@ const Cards = () => {
             repo: 'https://github.com/luissanchez92/CryptocurrencyQuoter',
             vista: 'https://cryptocurrencyljsj.netlify.app/'
         },
+        {
+            image: videoGames,
+            name: 'PI-VideoGames',
+            description: 'PI-VideoGames fue un proyecto individual durante la etapa del bootcamp que consistia en desarrollar una Single Page Application, que consistia en consumir y renderizar la informacion en de una API, incluye filtrados combinados, ordenamientos, creación de nuevos videojuegos con formulario controlado. ',
+            technologies: 'React, Axios, Node, Sequelize, PostgreSQL',
+            repo: 'https://github.com/luissanchez92/PI-VideoGames',
+            vista: ''
+        },
+        {
+            image: porfolio,
+            name: 'Mi Portfolio',
+            description: 'Explora mis habilidades y proyectos destacados. Use la integración de emailjs para que puedas contactarme fácilmente, completando el formulario desbloquearas el boton de envio. Espero conectarnos pronto.',
+            technologies: 'React',
+            repo: 'https://github.com/luissanchez92/portFolio',
+            vista: ''
+        }
     ]
     return (
         <div className={style.divContainer}>
